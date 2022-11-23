781039a7ebed9fdfe0e553f327a193a087a13359
Maruf_Monem <marufmonem@gmail.com>
2022-11-23 10:40:20 +0600

    Refactoring

--- src/main/java/RahulShettyTest.java
+++ src/main/java/RahulShettyTest.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
@@ -11,13 +12,12 @@ public class RahulShettyTest {
     @Test
     public void HomePageCheck() throws MalformedURLException {
         DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setBrowserName("firefox");
+//        caps.setBrowserName("firefox");
 //        Another format
-//        caps.setCapability(CapabilityType.BROWSER_NAME, "chrome");
+        caps.setCapability(CapabilityType.BROWSER_NAME, "firefox");
         WebDriver driver = new RemoteWebDriver(new URL("http://192.168.0.106:4444"),caps); //the hub URL and caps basically passes the requirements
 //        Then this goes to the distributor checks which machine can run chrome
         driver.get("https://rahulshettyacademy.com/");
-        driver.findElement(By.name("q")).sendKeys("Anik");
         System.out.println(driver.getTitle());
         driver.close();
 
